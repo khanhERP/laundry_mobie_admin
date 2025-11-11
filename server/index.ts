323652e8cfcd7680ae7b60bffc43eb6dc8c0305c
DESKTOP-54NCMET\PC-InfoPlus <truongpv@infoplusvn.com>
2025-11-11 13:23:11 +0700

    update

--- server/index.ts
+++ server/index.ts
@@ -218,7 +218,7 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
+  // if (app.get("env") === "development") {
   //   await setupVite(app, server);
   // } else {
   //   serveStatic(app);
